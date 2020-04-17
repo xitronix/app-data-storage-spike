ccfa5ac42007ca16e8ab366bed81341a76b397f2
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2020-04-17 15:29:12 +0300

    Add note

--- src/components/CredentialManagementApi/CredentialManagementApi.tsx
+++ src/components/CredentialManagementApi/CredentialManagementApi.tsx
@@ -11,7 +11,7 @@ export const CredentialManagementApi = () => {
       return alert('Your Browser does not support PasswordCredential')
     }
   }
-  
+
   async function storeCredential(profile: any) {
     ensurePasswordCredentialExist();
     const newCredential = new (window as any).PasswordCredential({
@@ -33,6 +33,10 @@ export const CredentialManagementApi = () => {
 
   return <>
     <h2> Credential Management API</h2>
+    <p style={{ backgroundColor: '#f6b73c' }}>
+      This API is restricted to top-level contexts. <br />
+      Calls to <i>get()</i> and <i>store()</i> within an <i>iframe</i> element will resolve <b>without effect</b>.
+    </p>
     <label>Credential: </label><input value={credential} onChange={event => setCredential(event.target.value)} />
     <button onClick={() => storeCredential(getAuth2().currentUser.get().getBasicProfile())}>Store credential</button>
     <button onClick={showStoredCredential}>Show stored credentials</button>
